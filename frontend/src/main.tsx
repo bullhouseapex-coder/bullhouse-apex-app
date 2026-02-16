87c95cf2af7e9793b109526d1bd8ee9c3eb4afa8
Marco Luis Hernandez <marcoluis.hernandez@gmail.com>
2026-02-16 17:39:03 +0800

    Security Fix

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,11 +6,9 @@ import { BrowserRouter } from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { AuthProvider } from "./contexts/AuthContext.tsx";
 
-const CLIENT_ID = "636890027947-c62m14a1rdpnqrpv2tg2fts6qlauad8l.apps.googleusercontent.com";
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId={CLIENT_ID}>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
       <BrowserRouter>
         <AuthProvider>
           <App />
